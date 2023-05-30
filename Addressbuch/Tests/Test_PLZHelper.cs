8c7f464f35e631be4f10a2e3990737169aa857e2
david <david@gangl-rastatt.de>
2023-05-30 20:12:31 +0200

    Tests

---
+++ Addressbuch/Tests/Test_PLZHelper.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Addressbuch.Tests
+{
+    [TestFixture]
+    public class PLZHelperTests
+    {
+        [Test]
+        public void PLZ_Finder_ValidInput_MatchingResults()
+        {
+            // Arrange
+            string input = "12345";
+
+            // Act
+            List<PlzData> results = PLZHelper.ReadEmbeddedCsvFile("plz_de")
+                .Where(x => x.PLZ == input)
+                .ToList();
+
+            // Assert
+            Assert.IsTrue(results.Count > 0);
+        }
+
+        [Test]
+        public void PLZ_Finder_InvalidInput_NoResults()
+        {
+            // Arrange
+            string input = "00000";
+
+            // Act
+            List<PlzData> results = PLZHelper.ReadEmbeddedCsvFile("plz_de")
+                .Where(x => x.PLZ == input)
+                .ToList();
+
+            // Assert
+            Assert.IsTrue(results.Count == 0);
+        }
+
+        [Test]
+        public void Ort_Finder_ValidInput_MatchingResults()
+        {
+            // Arrange
+            string input = "Berlin";
+
+            // Act
+            List<PlzData> results = PLZHelper.ReadEmbeddedCsvFile("plz_de")
+                .Where(x => x.Ort == input)
+                .ToList();
+
+            // Assert
+            Assert.IsTrue(results.Count > 0);
+        }
+
+        [Test]
+        public void Ort_Finder_InvalidInput_NoResults()
+        {
+            // Arrange
+            string input = "InvalidCity";
+
+            // Act
+            List<PlzData> results = PLZHelper.ReadEmbeddedCsvFile("plz_de")
+                .Where(x => x.Ort == input)
+                .ToList();
+
+            // Assert
+            Assert.IsTrue(results.Count == 0);
+        }
+    }
+}
---
+++ Addressbuch/Tests/Test_SearchEntryAddress.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Addressbuch.Tests
+{
+    [TestFixture]
+    public class SearchEntryAddressTests
+    {
+        [Test]
+        public void Search_WithValidAddress_ShouldPrintMatchingEntries()
+        {
+            // Arrange
+            string expectedEntry = "Max,Mustermann,Musterstraße 123,12345,Musterstadt,0123456789,01.01.2000,max@example.com,Musterfirma,Mustergruppe";
+            string searchQuery = "musterstraße";
+            using (StreamWriter writer = new StreamWriter("addressbook.txt"))
+            {
+                writer.WriteLine(expectedEntry);
+            }
+
+            StringWriter consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            // Act
+            SearchEntryAdress.Search();
+
+            // Assert
+            string consoleOutputString = consoleOutput.ToString();
+            Assert.IsTrue(consoleOutputString.Contains(expectedEntry));
+        }
+
+        [Test]
+        public void Search_WithEmptyAddress_ShouldDisplayErrorMessage()
+        {
+            // Arrange
+            string searchQuery = "";
+            StringWriter consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            // Act
+            SearchEntryAdress.Search();
+
+            // Assert
+            string consoleOutputString = consoleOutput.ToString();
+            Assert.IsTrue(consoleOutputString.Contains("Ungültige Eingabe! Bitte geben Sie eine Adresse ein."));
+        }
+
+        [Test]
+        public void Search_WithNoMatchingEntries_ShouldDisplayNoMatchMessage()
+        {
+            // Arrange
+            string expectedEntry = "Max,Mustermann,Musterstraße 123,12345,Musterstadt,0123456789,01.01.2000,max@example.com,Musterfirma,Mustergruppe";
+            string searchQuery = "test";
+            using (StreamWriter writer = new StreamWriter("addressbook.txt"))
+            {
+                writer.WriteLine(expectedEntry);
+            }
+
+            StringWriter consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            // Act
+            SearchEntryAdress.Search();
+
+            // Assert
+            string consoleOutputString = consoleOutput.ToString();
+            Assert.IsTrue(consoleOutputString.Contains("Kein passender Eintrag gefunden."));
+        }
+    }
+}
